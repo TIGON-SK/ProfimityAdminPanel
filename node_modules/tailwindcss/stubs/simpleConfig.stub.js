7528e5b21a1f1df0b7b7dc27e55d81282d99803a
Martn Hanák <martinhanak004@gmail.com>
2021-11-19 17:40:13 +0100

    prvy upload

---
+++ node_modules/tailwindcss/stubs/simpleConfig.stub.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'montserrat': ['Montserrat'],
+        'lato': ['Lato'],
+        'garamond': ['Garamond']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
